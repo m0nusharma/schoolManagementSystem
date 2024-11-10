471a71ae158e21e3f6cdbd58c24743752710d935
m0nusharma <sharmamonu42969@gmail.com>
2024-11-10 11:00:49 +0530

    heroSectionEdit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import HeroSection from './Components/HeroSection/HeroSection'
+
 
 function App() {
 
 
   return (
     <>
-   <h1 className="text-3xl font-bold underline p-5 text-red-500">
-      Hello world!
-    </h1>
+   <div>
+    <HeroSection/>
+   </div>
     </>
   )
 }
